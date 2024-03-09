ee6d9ebb033c8067a02a069e45dbee4256ba573a
Andrea Demetrio <andrea.demetrio@hotmail.it>
2024-03-09 12:12:17 +0100

    Added a little JS to the page to make content management easier

---
+++ extra_ui_elems.js
@@ -0,0 +1,27 @@
+function addTopBar(sourceDir='./', activeElement=-1){
+	let div = document.createElement('div');
+	div.classList.add('topnav');
+	const elems = ["Home", "Racconti", "Giochi", "Contatti"];
+	const links = ["index.html", "tales.html", "games.html", "contacts.html"];
+	for (let i = 0; i < elems.length; i++){
+		let linkElem = document.createElement('a');
+		let anchorText = document.createTextNode(elems[i]);
+		linkElem.appendChild(anchorText);
+		linkElem.title = elems[i];
+		linkElem.href = sourceDir + links[i];
+		if (activeElement == i){
+			linkElem.classList.add('active');
+		}
+		div.append(linkElem);
+	}
+	document.body.appendChild(div)
+}
+
+function addBottomBar(textToDisplay){
+	let div = document.createElement('div');
+	div.classList.add('bottomnav')
+	var paragraph = document.createElement("p");
+	paragraph.textContent = textToDisplay
+	div.append(paragraph);
+	document.body.appendChild(div)
+}
