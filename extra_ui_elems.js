8998305646ba5d659f10c2c5361a8c406efce946
Andrea Demetrio <andrea.demetrio@hotmail.it>
2025-03-22 16:10:12 +0100

    Add Ex-Lacrima Remannt and new CSS stuff

--- extra_ui_elems.js
+++ extra_ui_elems.js
@@ -25,3 +25,55 @@ function addBottomBar(textToDisplay){
 	div.append(paragraph);
 	document.body.appendChild(div)
 }
+
+function addBottomBarNoName(linkPrevious, titlePrevious, linkNext, titleNext, mainText){
+	let linkElemPrev = document.createElement('a');
+	let anchorTextPrev = document.createTextNode('< PREVIOUS');
+	linkElemPrev.appendChild(anchorTextPrev);
+	linkElemPrev.title = titlePrevious;
+	linkElemPrev.href = linkPrevious;
+	let linkElemNext = document.createElement('a');
+	let anchorTextNext = document.createTextNode('NEXT >');
+	linkElemNext.appendChild(anchorTextNext);
+	linkElemNext.title = titleNext;
+	linkElemNext.href = linkNext;
+	let div = document.createElement('div');
+	div.classList.add('bottomnav')
+	var paragraph = document.createElement("p");
+	paragraph.textContent = mainText
+	div.append(linkElemPrev);
+	div.append(paragraph);
+	div.append(linkElemNext);
+	document.body.appendChild(div)
+}
+
+function addBottomAllLinks(mainText, linkPrevious, titlePrevious, linkNext, titleNext, indexLink, indexText){
+	let linkElemPrev = document.createElement('a');
+	let anchorTextPrev = document.createTextNode('< PREVIOUS');
+	linkElemPrev.appendChild(anchorTextPrev);
+	linkElemPrev.title = titlePrevious;
+	linkElemPrev.href = linkPrevious;
+	let linkElemNext = document.createElement('a');
+	let anchorTextNext = document.createTextNode('    NEXT >');
+	linkElemNext.appendChild(anchorTextNext);
+	linkElemNext.title = titleNext;
+	linkElemNext.href = linkNext;
+	let div = document.createElement('div');
+	div.classList.add('bottomnav')
+	let linkElemIndex = document.createElement('a');
+	let anchorTextIndex = document.createTextNode('INDEX');
+	linkElemIndex.appendChild(anchorTextIndex);
+	linkElemIndex.title = indexText;
+	linkElemIndex.href = indexLink + "#TOC";
+	var paragraph = document.createElement("p");
+	var paragraph2 = document.createElement("p");
+	paragraph.textContent = mainText;
+	//paragraph.display = "block";
+	//paragraph.append(document.createElement('br'));
+	paragraph2.append(linkElemPrev);
+	paragraph2.append(linkElemIndex);
+	paragraph2.append(linkElemNext);
+	div.append(paragraph);
+	div.append(paragraph2);
+	document.body.appendChild(div)
+}
